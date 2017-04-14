490eca036d79ef16ed12faf129636287e841c609
Tom Wilson <tom@jackhq.com>
2017-04-14 10:22:50 -0400

    updated server to no longer support base64 buffer

--- server.js
+++ server.js
@@ -1,42 +1,43 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const http = require('http')
 const request = require('request')
 const HttpCors = require('./http-cors')
 const cors = new HttpCors({
-  origin: process.env.NODE_ENV === 'production' ? process.env.ORIGIN : 'http://localhost:3000'
+  origin: process.env.NODE_ENV === 'production'
+    ? process.env.ORIGIN
+    : 'http://localhost:3000'
 })
 
 const jwt = require('jsonwebtoken')
 
 const server = http.createServer(function (req, res) {
-
-  if (cors.apply(req, res)) return;
-  //get Bearer Header
-  var bearerToken;
-  var bearerHeader = req.headers["authorization"];
+  if (cors.apply(req, res)) return
+  // get Bearer Header
+  var bearerToken
+  var bearerHeader = req.headers['authorization']
   if (typeof bearerHeader !== 'undefined') {
-      var bearer = bearerHeader.split(" ");
-      bearerToken = bearer[1];
-      jwt.verify(bearerToken, new Buffer(process.env.AUTH0_SECRET, 'base64'), (err, decoded) => {
-        if (err) {
-          res.writeHead(403)
-          res.end(err.message)
-          return
-        }
-        req.pipe(request({
-          method: req.method,
-          url: process.env.DB_SERVER + req.url
-        })).pipe(res)
-      })
+    var bearer = bearerHeader.split(' ')
+    bearerToken = bearer[1]
 
+    jwt.verify(bearerToken, process.env.AUTH0_SECRET, (err, decoded) => {
+      if (err) {
+        res.writeHead(403)
+        res.end(err.message)
+        return
+      }
+      req
+        .pipe(
+          request({ method: req.method, url: process.env.DB_SERVER + req.url })
+        )
+        .pipe(res)
+    })
   } else {
-      res.writeHead(403)
-      res.end('Not Authorized!')
+    res.writeHead(403)
+    res.end('Not Authorized!')
   }
-
-
-
 }).on('error', function (err) {
   console.log(err)
 })
