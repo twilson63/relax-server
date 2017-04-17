912a47ec69dd2fa3a9f6775db4a92c9fd84f07a5
Tom Wilson <tom@jackhq.com>
2017-04-17 17:24:55 -0400

    modified server to receive secret, server and origin via command call

--- server.js
+++ server.js
@@ -1,45 +1,36 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
 const http = require('http')
 const request = require('request')
 const HttpCors = require('./http-cors')
-const cors = new HttpCors({
-  origin: process.env.NODE_ENV === 'production'
-    ? process.env.ORIGIN
-    : 'http://localhost:3000'
-})
 
 const jwt = require('jsonwebtoken')
 
-const server = http.createServer(function (req, res) {
-  if (cors.apply(req, res)) return
-  // get Bearer Header
-  var bearerToken
-  var bearerHeader = req.headers['authorization']
-  if (typeof bearerHeader !== 'undefined') {
-    var bearer = bearerHeader.split(' ')
-    bearerToken = bearer[1]
+module.exports = (jwt, server, origin) => {
+  const cors = new HttpCors({ origin: origin || 'http://localhost:3000' })
 
-    jwt.verify(bearerToken, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        res.writeHead(403)
-        res.end(err.message)
-        return
-      }
-      req
-        .pipe(
-          request({ method: req.method, url: process.env.SERVER + req.url })
-        )
-        .pipe(res)
-    })
-  } else {
-    res.writeHead(403)
-    res.end('Not Authorized!')
-  }
-}).on('error', function (err) {
-  console.log(err)
-})
+  return http.createServer(function (req, res) {
+    if (cors.apply(req, res)) return
+    // get Bearer Header
+    var bearerToken
+    var bearerHeader = req.headers['authorization']
+    if (typeof bearerHeader !== 'undefined') {
+      var bearer = bearerHeader.split(' ')
+      bearerToken = bearer[1]
 
-module.exports = server
+      jwt.verify(bearerToken, jwt, (err, decoded) => {
+        if (err) {
+          res.writeHead(403)
+          res.end(err.message)
+          return
+        }
+        req
+          .pipe(request({ method: req.method, url: server + req.url }))
+          .pipe(res)
+      })
+    } else {
+      res.writeHead(403)
+      res.end('Not Authorized!')
+    }
+  }).on('error', function (err) {
+    console.log(err)
+  })
+}
